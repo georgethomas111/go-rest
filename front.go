51dcc5696be40e91ded76ae83387f0f443f07bde
george <georgethomas.mec@gmail.com>
2013-10-22 00:26:01 +0530

    Adding commit

--- front.go
+++ front.go
@@ -1,1 +1,37 @@
-/home/george/work/odesk/progium/flynn/rest/src/myrest/front.go
+package main
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/rcrowley/go-tigertonic"
+)
+
+
+type MyResponse struct {
+	ID    string      `json:"id"`
+	Data interface{} `json:"trial"`
+}
+
+func DefaultResp() (*MyResponse) {
+	return	&MyResponse {
+		ID : "1",
+		Data : "Trial",
+		}
+}
+
+func trialHandler(u *url.URL, head http.Header, _ interface{}) (int, http.Header, *MyResponse, error) {
+	defResp := DefaultResp()
+	return http.StatusOK, nil, defResp, nil
+}
+
+func main() {
+
+	mux := tigertonic.NewTrieServeMux()
+	mux.Handle(
+		"GET",
+		"/trial",
+		tigertonic.Marshaled(trialHandler),
+	)
+	tigertonic.NewServer(":8000", tigertonic.Logged(mux, nil)).ListenAndServe()
+}
